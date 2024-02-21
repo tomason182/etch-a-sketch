f6ef754ad359d13f2c5c799384475c576263085c
tomason182 <elotro182@hotmail.com>
2024-02-21 10:29:10 -0300

    adding style in JS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//grid creator
+const container = document.querySelector('#container')
+container.setAttribute('style', 'border: solid; width: 100px; heigth: 100px')
+
+//create div
+
+const square = document.createElement('div')
+square.classList.toggle('grid-square')
+square.setAttribute('style', 'border: solid; width: 25px; heigth: 25px')
+square.style.width
