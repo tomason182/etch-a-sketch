2f5b32c7bc8f1e6d840d07f24c2ec7b4e51de86c
tomason182 <elotro182@hotmail.com>
2024-02-21 20:28:13 -0300

    handeling errors

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-let sizeInput = 16
+let sizeInput
+
+let gridSize; let blockSize; let numberOfIteration;
+[gridSize, blockSize, numberOfIteration] = handleSizeInput();
+
+console.log("gridSize: ", gridSize);
+console.log("blockSize: ", blockSize);
+console.log("number of iterations: ", numberOfIteration);
 
 const button = document.querySelector('#size');
 
 button.addEventListener('click', ()=> {
-    sizeInput = prompt('Insert grid size: ');
-
-    handleSizeInput(sizeInput);
+    sizeInput = parseInt(prompt('Insert grid size: '));
+    if (isNaN(sizeInput)) {
+        alert("Not a number");
+    }else if( sizeInput >= 1 && sizeInput <= 100){
+        handleSizeInput(sizeInput);
+    }else{
+        alert("Not in range")
+    }    
 })
 
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++){
+for (let i = 0; i < numberOfIteration; i++){
     const blocks = document.createElement('div');
     blocks.classList.toggle('blocks')
-    blocks.setAttribute('style', 'border: 1px solid black; width: 38px; height: 38px')
+    blocks.setAttribute('style', `border: 1px solid black; width: ${blockSize}px; height: ${blockSize}px`)
     
     blocks.addEventListener('mouseover', () => {
         blocks.style.backgroundColor = 'black';
@@ -23,7 +35,16 @@ for (let i = 0; i < 256; i++){
 }
 
 function handleSizeInput(sizeInput){
-    console.log('El valor ingresado es: ', sizeInput)
-}        
+    normalSize = 16;
+    if (sizeInput === undefined){
+        gridSize = normalSize;
+    }else{
+        gridSize = (normalSize*sizeInput);
+    }    
+    blockSize = (640/gridSize)-2;
+    numberOfIteration = Math.pow(gridSize, 2);
+
+    return [gridSize, blockSize, numberOfIteration];
+}
 
 
