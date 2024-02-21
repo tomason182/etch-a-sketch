7099b8684d5033b7fce79bd5ed88428cc38fa24c
tomason182 <elotro182@hotmail.com>
2024-02-21 10:48:50 -0300

    create div and append it

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const container = document.querySelector('#container')
-container.setAttribute('style', 'border: solid; width: 640px; height: 640px')
+const container = document.querySelector('#container');
+container.setAttribute('style', 'border: solid; width: 640px; height: 640px; display: flex');
 
 
 //create div
 
-const square = document.createElement('div')
-square.classList.toggle('grid-square')
-square.setAttribute('style', 'border: solid; width: 25px; height: 25px')
-square.style.width
+const square = document.createElement('div');
+square.classList.toggle('grid-square');
+square.setAttribute('style', 'border: solid; width: 40px; height: 40px');
+
+container.appendChild(square);
