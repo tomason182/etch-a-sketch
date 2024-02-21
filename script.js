07fc3a68112debc4bbb263a79af9dcd26d55604a
tomason182 <elotro182@hotmail.com>
2024-02-21 19:43:17 -0300

    callback funtion

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-function makeGrid() {
+let sizeInput = 16
 
-    const container = document.querySelector('#container');
-  
-    for (let i = 0; i < 256; i++){
-        const blocks = document.createElement('div');
-        blocks.classList.toggle('blocks')
-        blocks.setAttribute('style', 'border: 1px solid black; width: 38px; height: 38px')
-        
-        blocks.addEventListener('mouseover', () => {
-            blocks.style.backgroundColor = 'black';
-        });
-        
-        container.appendChild(blocks);
-    }
-        
+const button = document.querySelector('#size');
+
+button.addEventListener('click', ()=> {
+    sizeInput = prompt('Insert grid size: ');
+
+    handleSizeInput(sizeInput);
+})
+
+const container = document.querySelector('#container');
+
+for (let i = 0; i < 256; i++){
+    const blocks = document.createElement('div');
+    blocks.classList.toggle('blocks')
+    blocks.setAttribute('style', 'border: 1px solid black; width: 38px; height: 38px')
+    
+    blocks.addEventListener('mouseover', () => {
+        blocks.style.backgroundColor = 'black';
+    });
+    
+    container.appendChild(blocks);
 }
 
-makeGrid()
+function handleSizeInput(sizeInput){
+    console.log('El valor ingresado es: ', sizeInput)
+}        
+
+
