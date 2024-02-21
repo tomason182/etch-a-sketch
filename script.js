b8f73de598b3ed6cadbb0daee82e95cf36f9df9f
tomason182 <elotro182@hotmail.com>
2024-02-21 14:43:27 -0300

    replace fixed entries to variables

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+const norm = 16;
+let entry = 1;
+let size = norm * entry;
+let gridSize = Math.pow(size, 2)
+let pixel = 640/size
+
+console.log(size)
+console.log(pixel)
+console.log(gridSize)
+
+
 const container = document.querySelector('#container');
 container.setAttribute('style', 'display: flex; flex-flow: row wrap; max-width: 640px');
 
 
 //create div
 
-for (let i=0; i< 255; i++){
+for (let i=0; i< (gridSize - 1); i++){
     const square = document.createElement('div');
     square.classList.toggle('grid-square');
-    square.setAttribute('style', 'border: 1px solid grey; width: 40px; height: 40px;');
+    square.setAttribute('style', `border: 1px solid grey; width: ${pixel}px; height: ${pixel}px;`);
 
     container.appendChild(square);    
 }
@@ -15,13 +26,24 @@ for (let i=0; i< 255; i++){
 const hover = document.querySelectorAll('.grid-square');
 hover.forEach((sqr) => {
     sqr.addEventListener('mousemove', () => {
-        sqr.setAttribute('style', 'border: 1px solid grey; width: 40px; height: 40px; background-color: red')
+        sqr.setAttribute('style', `border: 1px solid grey; width: ${pixel}px; height: ${pixel}px; background-color: red`);
     });
 });
 
-const gridSize = document.querySelector('#size');
-gridSize.addEventListener('click', getPrompt)
+const gridSizeM = document.querySelector('#size');
+gridSizeM.addEventListener('click', getPrompt)
 
 function getPrompt() {
-    return alert(prompt("Enter a grid size - Max: 100"))
+    while (true) {
+        size = parseInt(prompt("Enter a grid size - min: 1 to max: 100"))
+        if (size > 100 ) {
+            alert("Incorrect Number");
+        }
+        else if (isNaN(size)) {
+            alert("Entry is not Integer");
+        }else{
+            break
+        } 
+        }
+    return size;           
 }
